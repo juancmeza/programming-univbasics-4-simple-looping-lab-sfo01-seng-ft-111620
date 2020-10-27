48389f510e4e9b6216064ce91a20c8d22ea0e62f
searching-block-4535 <juancmezat@gmail.com>
2020-10-27 07:43:33 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -30,7 +30,7 @@ end
 def return_string_array(array)
   count = 0 
   while count < array.length do 
-    array[count] = array[count].to_Str
+    array[count] = array[count].to_s
     count += 1 
   end
   array 
