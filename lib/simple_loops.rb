380ea3f7f74536a13a079985e8375c3c7dc1f80b
searching-block-4535 <juancmezat@gmail.com>
2020-10-27 07:12:15 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,19 @@
 # Write your methods here
+def loop_message_five_times(string)
+  count = 0 
+  while count < 6 do
+    puts string
+    count += 1
+  end
+end
+
+
+def loop_message_n_times(string, n)
+
+  count = 0 
+  while count < n do
+    puts string
+    count += 1 
+  end
+
+end 
